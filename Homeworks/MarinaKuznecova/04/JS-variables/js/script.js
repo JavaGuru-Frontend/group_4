dc049cd1ab0e87ee2f0d747aeb429b3cd809692d
Marina Kuznecova <satemka13@gmail.com>
2023-02-15 18:43:33 +0100

    Homework 04

---
+++ Homeworks/MarinaKuznecova/04/JS-variables/js/script.js
@@ -0,0 +1,15 @@
+/* в консоли записать */
+console.warn("Privet");
+var a = 5
+var b = 76
+
+let c = a + b
+let d = a - b
+let e = a * b
+let f = a / b
+
+console.log(" a + b = " + c);
+console.log(" a - b = " + d);
+console.log(" a * b = " + e);
+console.log(" a / b = " + f); 
+
