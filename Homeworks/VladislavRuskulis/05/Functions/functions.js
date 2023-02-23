bad6f1594120b9b7cffe71524b1581e6b83295f2
Vladislavs Ruskulis <ruksus228@gmail.com>
2023-02-23 12:43:49 +0200

    05-VladislavRuskulis homework

---
+++ Homeworks/VladislavRuskulis/05/Functions/functions.js
@@ -0,0 +1,35 @@
+
+let number = 100;
+let number2 = 5
+let proc = 35;
+
+
+function sum(number1, number2) {
+    const res = number1 + number2;
+    console.log(res)
+}
+function dif(number1, number2) {
+    const res = number1 - number2;
+    console.log(res)
+}
+function multi(number1, number2) {
+    const res = number1 * number2;
+    console.log(res)
+}
+function div(number1, number2) {
+    const res = number1 / number2;
+    console.log(res)
+}
+function percent(number, proc) {
+    const res = number / 100 * proc;
+    console.log(res)
+}
+
+sum(number, number2)
+dif(number, number2)
+multi(number, number2)
+div(number, number2)
+percent(number, proc)
+
+
+
