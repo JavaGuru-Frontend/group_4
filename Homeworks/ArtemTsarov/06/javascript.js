edb20d04b727286fc59ed3fb5a478aedb22015fc
Artem Tsarov <artemtsarev45@gmail.com>
2023-02-25 16:58:39 +0200

    06Homework

---
+++ Homeworks/ArtemTsarov/06/javascript.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded',() => {
+    alert("JavaScript Подключён");
+
+document.getElementById("h1");
+document.getElementById("h4");
+
+document.getElementById("h1").innerText = "text";
+});
+
