655e4b98ac2fe724906adeb8882021f26a430908
pjotrs olenovics <p.olenovics@inbox.lv>
2023-03-01 22:30:51 +0200

    js/04

---
+++ Homeworks/PjotrsOlenovics/04/JS-variables/script.js
@@ -0,0 +1,14 @@
+let a = 40;
+let b = 10;
+
+let plus = a + b
+console.log(plus);
+
+let minus = a - b
+console.log(minus);
+
+let multi = a * b
+console.log(multi);
+
+let divide = a / b
+console.log(divide);
