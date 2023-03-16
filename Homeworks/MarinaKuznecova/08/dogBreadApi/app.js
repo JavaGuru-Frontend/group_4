26e35fd1474d8a64826950b38e7123df729a47dc
Voronovs Jevgenijs, SUB-SDC-LV-DOS-CSA <Jevgenijs.Voronovs@swisscom.com>
2023-03-16 16:42:29 +0200

    adjusttemnst

--- Homeworks/MarinaKuznecova/08/dogBreadApi/app.js
+++ Homeworks/MarinaKuznecova/08/dogBreadApi/app.js
@@ -6,6 +6,12 @@ let url = '';
 
 function createBreedList() {
     let htmlDogs = data.map(function (breed, i) {
+        // zeka MarinaKuznecova8 
+        // этот кусок кода будет работать но мы могли бы избавиться от лишней проверки 
+        // if (i === 0)
+        // мы могли бы поступить следующим образом
+        // перенести <option>Choose a dog breed</option> в html 
+        // и дальше просто подставить все небходимые нам опции через +=
         if (i === 0) {
             return `<option>Choose a dog breed</option>` +                 
                 `<option value ="${breed.value}">${breed.name} </option>`; 
@@ -21,13 +27,14 @@ function renderDogData(data) {
 }
 
 function getBreedImg() {
+    // zeka MarinaKuznecova8 
+    // замечательный кейс проверить что мы действительно что то выбрали
     if (dogSelector.value !== 'Choose a dog breed') {                     
         url = apiURL + `breed/${dogSelector.value}/images/random`;        
         fetch(url)
             .then(response => response.json())
             .then(data => renderDogData(data))
             .catch(error => alert(error));
-
     } else {
         dogImg.innerHTML = '';
     }
