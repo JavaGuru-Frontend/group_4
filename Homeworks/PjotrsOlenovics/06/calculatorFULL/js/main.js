3da6501cc94ebcbf3f7e1689a29cae9d580a882b
pjotrs olenovics <p.olenovics@inbox.lv>
2023-03-22 23:12:37 +0200

    calc ful

--- Homeworks/PjotrsOlenovics/06/calculatorFULL/js/main.js
+++ Homeworks/PjotrsOlenovics/06/calculatorFULL/js/main.js
@@ -1,3 +1,72 @@
-// У нас уже приготовлена вёрстка для калькулятора
-// нам необходимо добавить нужные addEventListener на кнопки и добавить функции которые будут вызываться на нажатие этих кнопок
-// Результат вычеслений вывести в поле output используя метод innerHTML
+const output = document.getElementById("output");
+const buttons = document.querySelectorAll(".btn");
+
+let currentNumber = "";
+let operator = "";
+let previousNumber = "";
+let result = "";
+
+function addNumber(number) {
+  currentNumber += number;
+  output.innerHTML = currentNumber;
+}
+
+function selectOperator(selectedOperator) {
+  operator = selectedOperator;
+  previousNumber = currentNumber;
+  currentNumber = "";
+}
+
+function clearAll() {
+  currentNumber = "";
+  operator = "";
+  previousNumber = "";
+  output.innerHTML = "0";
+}
+
+function calculate() {
+  switch (operator) {
+    case "+":
+      result = parseFloat(previousNumber) + parseFloat(currentNumber);
+      break;
+    case "-":
+      result = parseFloat(previousNumber) - parseFloat(currentNumber);
+      break;
+    case "*":
+      result = parseFloat(previousNumber) * parseFloat(currentNumber);
+      break;
+    case "/":
+      result = parseFloat(previousNumber) / parseFloat(currentNumber);
+      break;
+  }
+  output.innerHTML = result;
+  previousNumber = "";
+  currentNumber = result.toString();
+}
+
+function handleButtonClick(e) {
+  const type = e.target.dataset.type;
+  const id = e.target.dataset.id;
+
+  switch (type) {
+    case "number":
+      addNumber(id);
+      break;
+    case "operator":
+      selectOperator(id);
+      break;
+    case "equal":
+      calculate();
+      break;
+    case "clear":
+      clearAll();
+      break;
+    case "symbol":
+
+      break;
+  }
+}
+
+buttons.forEach(button => {
+  button.addEventListener("click", handleButtonClick);
+});
